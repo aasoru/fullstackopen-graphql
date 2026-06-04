54e6ccc18e9111f93b0a3efc798c3d350a33f433
Aarón Sosa <aasoru@gmail.com>
2026-06-04 22:02:24 +0100

    exercise 3

--- library-backend/index.js
+++ library-backend/index.js
@@ -80,6 +80,13 @@ let books = [
 ]
 
 const typeDefs = /* GraphQL */ `
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
   type Book {
     title: String!
     author: String!
@@ -92,6 +99,7 @@ const typeDefs = /* GraphQL */ `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -100,6 +108,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length,
   },
 }
 
